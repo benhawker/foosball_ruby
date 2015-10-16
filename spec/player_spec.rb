d84508634edd61be0adb5e80e50faaa89b182350
Ben <ben.c.hawker@gmail.com>
2015-10-16 15:38:58 +0100

    Added basic player.new and test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,16 @@
 require "player"
 
 describe Player do
+  subject(:player) {Player.new("ben", "hawker")}
+
+  describe "player attributes" do
+    it "should have a firstname" do
+      expect(player.first_name).to eq "ben"
+    end
+
+    it "should have a firstname" do
+      expect(player.last_name).to eq "hawker"
+    end
+  end
 
 end
